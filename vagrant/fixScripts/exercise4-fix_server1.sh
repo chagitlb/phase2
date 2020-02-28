df7058b6623502370fa151bbfbc1c0628c123281
chagitlb <61159734+chagitlb@users.noreply.github.com>
2020-02-29 01:47:51 +0200

    hotfixes

--- vagrant/fixScripts/exercise4-fix_server1.sh
+++ vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo sed -i '$ a 192.168.100.11 server2' /etc/hosts
-yes | exercise4-fix_server1.sh 
+yes ""
--- vagrant/fixScripts/exercise4-fix_server2.sh
+++ vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo sed -i '$ a 192.168.100.10 server1' /etc/hosts
-yes | exercise4-fix_server2.sh
+yes

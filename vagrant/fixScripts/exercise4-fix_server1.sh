5f504941fe07fde2a7d698d2fe227d767887a299
chagitlb <61159734+chagitlb@users.noreply.github.com>
2020-02-29 18:46:25 +0200

    debug

--- vagrant/fixScripts/exercise4-fix_server1.sh
+++ vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+echo "debug1"
 chmod a+x exercise4-fix_server1.sh
 echo '192.168.100.11 server2' | sudo tee /etc/hosts -a
+"echo "debug2"
--- vagrant/fixScripts/exercise4-fix_server2.sh
+++ vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-chmod a+x exercise4-fix_server2.sh
+echo "debug12"
 echo '192.168.100.10 server1' | sudo tee /etc/hosts -a
+echo "debug "22"

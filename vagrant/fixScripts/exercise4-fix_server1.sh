955fa1e81ac006fe3c6cdb6b052675ecbfe50002
chagitlb <61159734+chagitlb@users.noreply.github.com>
2020-02-29 16:05:04 +0200

    4

--- vagrant/fixScripts/exercise4-fix_server1.sh
+++ vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+chmod a+x exercise4-fix_server1.sh
 echo '192.168.100.11 server2' | sudo tee /etc/hosts -a
--- vagrant/fixScripts/exercise4-fix_server2.sh
+++ vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+chmod a+x exercise4-fix_server2.sh
 echo '192.168.100.10 server1' | sudo tee /etc/hosts -a

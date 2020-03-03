27691271d70b7e3248618bc3394ceac155083717
chagitlb <61159734+chagitlb@users.noreply.github.com>
2020-03-03 16:27:48 +0200

    Update exercise5-fix_server1.sh

--- vagrant/fixScripts/exercise5-fix_server1.sh
+++ vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-ssh-keygen -t rsa
-ssh-copy-id -i ~/.ssh/id_rsa.pub vagrant@192.168.100.11;;expect password;send "vagrant";interact
+ssh-copy-id -i ~/.ssh/id_rsa.pub vagrant@192.168.100.11
 echo -e "vagrant\n"
 yes "yes" | "Are you sure you want to continue connecting (yes/no)?"
 ssh vagrant@192.168.100.11
